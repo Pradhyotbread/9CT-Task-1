360f06cc9bcab5368da144947849faedbce420c1
bread <pradhyot.narasimha@education.nsw.gov.au>
2026-05-01 12:30:45 +1000

    csz

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import pandas as pd
 import time 
 import sys 
 
-NCISdf = pd.read_csv('Data/NCIS_Background_Checks.csv', on_bad_lines="skip")
+NCISdf = pd.read_csv('U:\computingTech\9CT-Task-1\ViolentCrimeData.csv', on_bad_lines="skip")
 print(NCISdf)
