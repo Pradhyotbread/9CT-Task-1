17812d988d5b2c0c4438504bed4fb3307dc0f63a
bread <pradhyot.narasimha@education.nsw.gov.au>
2026-05-07 09:22:28 +1000

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import pandas as pd
 import time 
 import sys 
 
-NCISdf = pd.read_csv('C:\Users\pvnpk\Documents\GitHub\9CT-Task-1\F1Drivers_Dataset.csv', on_bad_lines="skip")
-print(NCISdf)
+F1df = pd.read_csv("U:\computingTech\9CT-Task-1\F1Drivers_Dataset.csv", on_bad_lines="skip")
+print(F1df)
