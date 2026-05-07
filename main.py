8332d069ba5205e606033931d451193cce984198
bread <pradhyot.narasimha@education.nsw.gov.au>
2026-05-07 10:09:46 +1000

    g

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 import time 
 import sys 
 
-F1df = pd.read_csv("U:\computingTech\9CT-Task-1\F1Drivers_Dataset.csv", on_bad_lines="skip")
+F1df = pd.read_csv("F1Drivers_Dataset(in).csv")
 print(F1df)
