523f80b586a1618079a2c060464e4ab3c149c6f6
bread <pradhyot.narasimha@education.nsw.gov.au>
2026-05-08 15:05:37 +1000

    se4

--- Data_module.py
+++ Data_module.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
+F1db = pd.read_csv('U:\computingTech\9CT-Task-1\F1Drivers_Dataset(in) (1).csv', on_bad_lines='skip')
 
 #---Functions---------------------------------------------------------------------------------------------------------#
 
 def Data_Visualisation():
-    F1db = pd.read_csv('U:\computingTech\9CT-Task-1\F1Drivers_Dataset(in) (1).csv', on_bad_lines='skip')
+    F1db(header=None, 
+                          names=['Name', 'nationality', 'points'])
+   
     print(F1db)
+Data_Visualisation()
