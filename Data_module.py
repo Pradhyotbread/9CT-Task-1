51dd1a7a2ea0b992441a72f7fdaad194d41bee65
bread <pradhyot.narasimha@education.nsw.gov.au>
2026-05-12 14:46:05 +1000

    Update Data_module.py

--- Data_module.py
+++ Data_module.py
@@ -4,8 +4,37 @@ F1db = pd.read_csv('U:\computingTech\9CT-Task-1\F1Drivers_Dataset.csv', on_bad_l
 
 #---Functions---------------------------------------------------------------------------------------------------------#
 
-def Data_Visualisation():
-    DriverOrigin = F1db.drop(columns='')
-   
-    print(F1db)
-Data_Visualisation()
+def driverNational():
+    DriverOrigin = F1db.drop(columns=['Championships', \
+    'Race_Entries', \
+    'Race_Starts', \
+    'Pole_Positions', \
+    'Race_Wins', \
+    'Podiums', \
+    'Points', \
+    'Decade', \
+    'Pole_Rate', \
+    'Win_Rate', \
+    'Podium_Rate', \
+    'Years_Active'])
+    print(DriverOrigin)
+
+def driverYear():
+    driverDecade = F1db.drop(columns=['Championships', \
+    'Race_Entries', \
+    'Race_Starts', \
+    'Pole_Positions', \
+    'Race_Wins', \
+    'Podiums', \
+    'Points', \
+    'Nationality', \
+    'Pole_Rate', \
+    'Win_Rate', \
+    'Podium_Rate', \
+    'Years_Active'])
+    print(driverDecade)
+driverYear()
+
+def dataVisual1():
+    F1db.plot(kind='bar', x='Driver', y='Points')
+dataVisual1()
